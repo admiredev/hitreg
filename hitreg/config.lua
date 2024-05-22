511ca68b3f36f77bc5d5845b00aea6807c4091e4
Zae <134421936+admiredev@users.noreply.github.com>
2024-05-22 15:33:18 -0400

    Delete hitreg directory

--- hitreg/config.lua
+++
@@ -1,6 +0,0 @@
-Config = {}
-Config.IgnoreGodmode = true
-Config.ExcludedWeapons = {
-    [`WEAPON_UNARMED`] = true,
-    [`WEAPON_STUNGUN`] = true,
-}
--- hitreg/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
-author 'Zae / Discord:ripadmire'
-
-server_scripts {
-    'config.lua', 
-    'server.lua'
-}
