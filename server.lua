45404e889bf0b961fb03d94fec65999500fb20de
Zae <134421936+admiredev@users.noreply.github.com>
2024-05-22 15:34:54 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,30 +1,29 @@
 local function handleWeaponDamage(sender, data)
     local senderPed = GetPlayerPed(sender)
-
-	if data.hitComponent ~= 20 or (not IsPedAPlayer(senderPed)) then
+    if data.hitComponent ~= 20 or (not IsPedAPlayer(senderPed)) then
         return
     end
-    
-	local weapon = GetSelectedPedWeapon(senderPed)
-	if Config.ExcludedWeapons[weapon] then
-		return
-	end
 
-	local targetPed = NetworkGetEntityFromNetworkId(data.hitGlobalId)
-	local targetId = NetworkGetEntityOwner(targetPed)
-	if not targetId or not IsPedAPlayer(targetPed) or GetEntityHealth(targetPed) <= 0 then
-		return
-	end
+    local weapon = GetSelectedPedWeapon(senderPed)
+    if Config.ExcludedWeapons[weapon] then
+        return
+    end
 
-	if Config.IgnoreGodmode and GetPlayerInvincible(targetId) then
-		return
-	end
+    local targetPed = NetworkGetEntityFromNetworkId(data.hitGlobalId)
+    local targetId = NetworkGetEntityOwner(targetPed)
+    if not targetId or not IsPedAPlayer(targetPed) or GetEntityHealth(targetPed) <= 0 then
+        return
+    end
+
+    if Config.IgnoreGodmode and GetPlayerInvincible(targetId) then
+        return
+    end
 
     SetPedConfigFlag(targetPed, 438, true)
-	SetPedCanRagdoll(targetPed, true)
+    SetPedCanRagdoll(targetPed, true)
     SetPedConfigFlag(targetPed, 33, true)
     SetPedToRagdoll(targetPed, -1, -1, 0, 0, 0, 0)
     SetPedConfigFlag(targetPed, 33, false)
 end
 
-AddEventHandler('weaponDamageEvent', handleWeaponDamage)
+AddEventHandler("weaponDamageEvent", handleWeaponDamage)
